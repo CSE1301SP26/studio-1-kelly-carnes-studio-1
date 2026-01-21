48c12033bbf20501a4907a8cf9512f5e7a926135
SarahGKelly620 <Kelly.s.g@wustl.edu>
2026-01-21 15:42:52 -0600

    Studio 1 finsihed

---
+++ src/studio1/Average.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is the first number?");
+        int n1 = in.nextInt();
+        System.out.println("What is the second number?");
+        int n2 = in.nextInt();
+        double avg = (n1 + n2) / 2.01;
+        System.out.print(avg);
+    }
+
+}
